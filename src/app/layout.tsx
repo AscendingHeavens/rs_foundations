8842621c29578dbc73ba4aa8696f78c9f27e3a2a
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-11-10 18:30:14 +0530

    improve landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,14 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "next-themes";
 import "./globals.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { Navbar } from "@/components/Navbar";
+import { Footer } from "@/components/Footer";
+import { PopupWidget }  from "@/components/PopupWidget";
+
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,16 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
-        <div className="min-h-dvh">
-
-        {children}
-        </div>
-        <Footer />
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class">
+          <Navbar />
+          <div>{children}</div>
+          <Footer />
+          <PopupWidget />
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-type Navlinks = {
-    title: string;
-    href: string;
-    };
